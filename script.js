76ac3adaf4a34c7e3f53512a58308961846f1465
greenturtle537 <greenturtle537@mail.com>
2023-11-24 22:01:58 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const url = 'http://glitchtech.top:8/connect?username=me';
+fetch(url)
+.then((response) => {
+  
+})
+.then((data) => {
+  console.log(data)
+})
+.catch(function(error) {
+  console.log(error);
+});
